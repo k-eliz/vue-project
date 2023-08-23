a29ad5e416f0fcd1c13b6e0a92aa1e17b687368d
Elizaveta Kulinich <kulinich.eliz@gmail.com>
2023-08-23 19:36:52 +0300

    finished project

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,85 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('store-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoreResources from "./StoreResources.vue";
+import AddResource from "./AddResource.vue";
+export default {
+  components: {
+    StoreResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: "stored-resources",
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://www.google.com",
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
+      deleteResource: this.removeResource,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === "store-resources" ? null : "flat";
+    },
+    addResButtonMode() {
+      return this.selectedTab === "add-resource" ? null : "flat";
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = "store-resources";
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(
+        (res) => res.id === resId
+      );
+      this.storedResources.splice(resIndex, 1);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
